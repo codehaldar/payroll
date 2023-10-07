26f0b5338e91e9f6e1e1f74d120b1fe698158781
codehaldar <subhamhaldar2001@gmail.com>
2023-10-07 17:19:57 +0530

    Abstruct Base Employee class is created

---
+++ Payroll.java
@@ -0,0 +1,21 @@
+package com.company;
+abstract class Employee{
+    private String name;
+    private int id;
+    public Employee(String name, int id){
+        this.name=name;
+        this.id=id;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getId(){
+        return id;
+    }
+};
+public class Payroll {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
